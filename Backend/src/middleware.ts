115192764c9d6f9a6f923a2ba40572e49b5606ad
raunit-dev <raunitjaiswal510@gmail.com>
2025-06-17 23:26:57 +0530

    pushing some changes

--- Backend/src/middleware.ts
+++ Backend/src/middleware.ts
@@ -1,2 +1,16 @@
 import { NextFunction, Request, Response } from "express";
-import { JSONSerialized } from "mongoose";
+import { JWT_SECRET } from "./config";
+import jwt from "jsonwebtoken";
+
+export const userMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    const header = req.headers["authorization"];
+    const decoded = jwt.verify(header as string, JWT_SECRET);
+
+    if (decoded) {
+        // @ts-ignore
+        req.userId = decoded.id;
+        next();
+    } else {
+        res.status(401).json({ message: "Unauthorized User" });
+    }
+};
